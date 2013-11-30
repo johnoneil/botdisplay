590fd68f5c110adeb89bbe1b29dedca81577d82a
johnoneil <oneil.john@gmail.com>
2013-11-30 13:58:29 -0800

    Added some static files for test display. Revised timeout handling in page showing.

--- botdisplay.py
+++ botdisplay.py
@@ -14,28 +14,48 @@ DATE: Thursday, November 7th 2013
 
 
 from selenium import webdriver
-#from selenium.common.exceptions import TimeoutException
-#from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 import time
 import argparse
-#import threading
-#import time
+import os
 import concurrent.futures
 
+def local_file(relative_url):
+  '''
+  :arg relative_url: relative url to local file
+  :type relative_url: string (should be path to existing file)
+  :returns absolute URL suitable for browser
+  '''
+  return 'file://'+os.path.abspath(relative_url)
 
-driver = None
 
-def close():
-  global driver
-  print 'About to try closing browser.'
-  if driver:
-    print 'Attempting to close browser.'
-    driver.quit()
-    driver = None
+def show_page(browser, url):
+  '''
+  Drive browser to page url
 
-def show_page(browser, url, time_to_display=30.0):
+  :arg browser: instance of selenium webdriver
+  :type browser: selenium.webdriver
+  :arg url: url to point browser to
+  :type url: string
+
+  '''
+  browser.get(url)
+
+def timeout(seconds):
+  '''
+    Simple wrapper for sleep call, meant to be used via concurrency
+
+    :arg seconds: time to sleep in seconds
+    :type seconds: int
+  '''
+  time.sleep(seconds)
+
+def show_page_with_timeout(thread_pool, browser, url, time_to_display):
   '''
   Drive browser to page url for x seconds
+  With parallel timer thread killing display if browser.get()
+  never completes (due to page not loading?)
 
   :arg browser: instance of selenium webdriver
   :type browser: selenium.webdriver
@@ -45,39 +65,53 @@ def show_page(browser, url, time_to_display=30.0):
   :type time_to_display: float
 
   '''
-  browser.get(url)
-  time.sleep(time_to_display)
-  print('Showed {} for {} seconds'.format(url, time_to_display))
-  return sleepTime
+  show_page(browser, url)
+  timer_future = thread_pool.submit(timeout, time_to_display)
 
-def async_show_page(thread_pool, browser, url, time_to_display=30.0):
+  while not timer_future.done():
+    pass
+
+  return time_to_display
+
+def async_show_page_with_timeout(thread_pool, browser, url, time_to_display):
   '''
-  Drive browser to page asynchronously
-
-  TODO: this could be revised to be method decorator
-
-  :returns concurrency.future of task completion
+  Show a page, timing out if it doesn't load or something.
+  
+  arg: thread_pool: concurrency.threadpool to add tasks to
+  arg: browser: selenium browser remote control to manipulate
+  arg: url: standard URL to resource appropriate for browser
+  arg: time_to_display: time in seconds to keep page up
   '''
-  future = thread_pool.submit(show_page, browser, url, time_to_display)
+  future = thread_pool.submit(show_page_with_timeout, thread_pool, browser, url, time_to_display)
   return future
   
 def main():
   #TODO: move towards DB driven list of urls and other events
   #TODO: embed in cyclone(?) web app framework
+  #TODO: regex check for youtube links and reformat them as:
+  #original link: http://www.youtube.com/v/GsF1jnTjRLo
+  #fullscreen autoplaying link: https://youtube.googleapis.com/v/GsF1jnTjRLo%26hd=1%20%26autoplay=1
+  #where  '&autoplay=1' specifies autoplay and  '&hd=1' is hd version,
+  #and using youtube.googleapis.com goes fullscreen
   urls = [
+      #'http://192.168.1.3/',
+      local_file('./media/spooky_skeleton_1.gif'),
       'http://nyaa.eu/',
+      'http://youtube.googleapis.com/v/a_6CZ2JaEuc&autoplay=1',
       'http://drudgereport.com/',
+      local_file('./media/smiling_white_dog.jpg'),
       'http://google.com'
     ]
 
   #spin off tasks asynchronously
-  with concurrent.futures.ThreadPoolExecutor(max_workers=5) as thread_pool:
+  with concurrent.futures.ThreadPoolExecutor(max_workers=10) as thread_pool:
     #browser =  webdriver.chrome()
     browser =  webdriver.Firefox()
   
     for url in urls:
-      future = async_show_page(thread_pool, browser, url, time_to_display=10.0)
-      while future.running():
+      print 'opening ' + url
+      future = async_show_page_with_timeout(thread_pool, browser, url, time_to_display=10.0)
+      while not future.done():
         pass#we could be doing something here.
 
     browser.quit()
