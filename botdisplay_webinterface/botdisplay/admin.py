6fbf1ed42d9b8fb8ecf00c4a231ae1d7128b5f7c
johnoneil <oneil.john@gmail.com>
2013-12-09 20:46:28 -0800

    Autopopulate user and date of database entry creation.

--- botdisplay_webinterface/botdisplay/admin.py
+++ botdisplay_webinterface/botdisplay/admin.py
@@ -3,6 +3,10 @@ from botdisplay.models import URLDisplay
 from django.contrib import admin
 
 class BotdisplayAdmin(admin.ModelAdmin):
-	list_display = ('url', 'display_time', 'creation_date','created_by')
+	#list_display = ('url', 'display_time')
+  exclude = ('created_by',)
+  def save_model(self, request, obj, form, change):
+    obj.created_by = request.user
+    obj.save()
 
 admin.site.register(URLDisplay, BotdisplayAdmin)
--- botdisplay_webinterface/botdisplay/models.py
+++ botdisplay_webinterface/botdisplay/models.py
@@ -1,14 +1,13 @@
 # vim: set ts=2 expandtab:
 from django.db import models
+from django.contrib.auth.models import User
 import datetime
 
 class URLDisplay(models.Model):
   url = models.CharField(max_length=2048)
   display_time = models.IntegerField()
-  creation_date = models.DateTimeField('date published')
-  created_by = models.CharField(max_length=200)
+  creation_date = models.DateTimeField('date published', auto_now_add=True)
+  created_by = models.ForeignKey(User, related_name='+')
   def __unicode__(self):
     return self.url
-  def was_published_today(self):
-    return self.creation_date.date() == datetime.date.today()
 
