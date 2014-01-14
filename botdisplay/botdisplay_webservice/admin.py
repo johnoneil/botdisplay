0543e0ab98b12397ea53e29c95dbef2eef5b8066
johnoneil <oneil.john@gmail.com>
2014-01-13 23:25:13 -0800

    Correction to admin interface to botdisplay model

--- botdisplay/botdisplay_webservice/admin.py
+++ botdisplay/botdisplay_webservice/admin.py
@@ -1,4 +1,11 @@
+# vim: set ts=2 expandtab:
 from django.contrib import admin
 from botdisplay.botdisplay_webservice.models import URLDisplay
 
-admin.site.register(URLDisplay)
+class BotdisplayAdmin(admin.ModelAdmin):
+  exclude = ('created_by',)
+  def save_model(self, request, obj, form, change):
+    obj.created_by = request.user
+    obj.save()
+
+admin.site.register(URLDisplay, BotdisplayAdmin)
